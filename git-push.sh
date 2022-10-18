f52ed833a4abd15d8225ab6b48d21de23d013ca9
sagar118 <sgrhacker18@gmail.com>
2022-10-18 14:42:12 -0400

    "New changes to script"

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+quarto render
+
 git add -u
 
 echo 'Enter the commit message:'
@@ -10,16 +12,16 @@ git commit -m "$commitMessage"
 echo '\nEnter the name of the branch:'
 read branch
 
-if git merge-base --is-ancestor origin/$branch $branch; then
-    echo Empty
-else
+if ! (git merge-base --is-ancestor origin/$branch $branch); then
     echo "\nPerforming rebase!"
     git pull origin $branch --rebase
 fi
 
+git status
+
 if [[ "$(git push --porcelain)" == *"Done"* ]];
 then
-    echo "\nIt is clean"
+    echo "\nGit push was successful"
 else
     echo "\nPushing data to remove server!!"
     git push origin $branch
